52f89595d1bdf85825649f39915a8f27c317df5a
dadanili <yli523@gmail.com>
2016-09-25 15:26:37 -0700

    fixed hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,29 @@
-var webpack = require('webpack');
 var path = require('path');
-  console.log(__dirname,'dirname')
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var webpack = require('webpack');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
-var config = {
-  entry: ['react-hot-loader/patch',
-  'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
-  'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors,
-  APP_DIR + '/index.jsx'],
+console.log('=============', path.join(__dirname, 'dist'))
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    APP_DIR + '/index.jsx'
+  ],
   output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel',
-        // query:
-        //   {
-        //     presets:['react']
-        //   }
-      },
-
-      // { 
-      //   test: /\.jsx?$/, 
-      //   loaders: ['react-hot'], 
-      //   include: path.join(__dirname, 'src') 
-      // }
-
-    ]
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
-  ]
+  ],
+  module: {
+    loaders: [
+    {
+      test: /\.jsx?/,
+      loaders: ['react-hot', 'babel'],
+      include: APP_DIR
+    }]
+  }
 };
-
-module.exports = config;
