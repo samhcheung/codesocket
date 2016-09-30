2a7e04331db2a5712c7809a4468a034ec8324fae
samhcheung <samhcheung@gmail.com>
2016-09-29 17:37:22 -0700

    borders

--- src/client/app/nav/presentation.js
+++ src/client/app/nav/presentation.js
@@ -3,16 +3,25 @@ import {Link} from 'react-router';
 
 const navstyle = {
   mainheader: {
-    padding:'15px'
+    //height:'60px',
+    padding:'15px',
+    background:'rgb(244, 244, 244)'
+  },
+  navbuttons: {
+    padding:'15px',
+    "border-style": 'solid',
+    "border-width": '0.5px',
+    "border-color":'rgb(199, 199, 199)',
+    "margin-bottom": '10px'
   }
 }
 const NavPresentation = (props) => {
     return (
       <div className='loadingscreen'>
-        <div className="row">
-          <div className="col-sm-3" style={navstyle.mainheader}>Code Socket</div>
+        <div className="row" style={navstyle.mainheader}>
+          <div className="text-center" >Code Socket</div>
         </div>
-        <div className="row">
+        <div className="row" style={navstyle.navbuttons}>
           <div className="col-sm-3 text-center"><Link >Rooms</Link></div>
           <div className="col-sm-3 text-center" onClick={e=>props.addDoc(e)}>Add New Doc</div>
           <div className="col-sm-3 text-center">History</div>
