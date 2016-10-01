86235e685bca90daccdbdea753f53c89514ca337
dadanili <yli523@gmail.com>
2016-10-01 11:30:47 -0700

    conflict in container resolved

--- src/client/app/reducers/sessionreducer.js
+++ src/client/app/reducers/sessionreducer.js
@@ -1,11 +1,8 @@
 const sessionInitialState = {
   socket : null,
   room: '',
-<<<<<<< 703f9f414a22cb87b0027cce6326d398340d5bfa
-  quill: null
-=======
+  quill: null,
   modalopen: false
->>>>>>> wired up React Modal to surface join room popup
 }
 
 export default function sessionReducer (state = sessionInitialState, action) {
@@ -22,19 +19,17 @@ export default function sessionReducer (state = sessionInitialState, action) {
        room: action.room
      }
     }
-<<<<<<< 703f9f414a22cb87b0027cce6326d398340d5bfa
     case 'UPDATE_QUILL' : {
       return {
         ...state,
         quill: action.quill
       }
-=======
+    }
     case 'DOC_SELECTION_MODAL' : {
      return {
        ...state, 
        modalopen: action.modalopen
      }
->>>>>>> wired up React Modal to surface join room popup
     }
     default : {
      return state
