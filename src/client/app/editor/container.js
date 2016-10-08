ae2f8db6108a59cc83f0d41a7ac2c1e565d400a1
Stephen Leung <stephenjleung@gmail.com>
2016-10-08 16:31:06 -0700

    Refactor collab serverState to an object for room management.
    Adjust setInterval logic to clearInterval first before starting
    a new one.

--- src/client/app/editor/container.js
+++ src/client/app/editor/container.js
@@ -35,7 +35,7 @@ class EditorContainer extends React.Component {
     // var socket = io();
     var quill = new Quill('#editor', {
         modules: {
-        syntax: true,              // Include syntax module
+        // syntax: true,              // Include syntax module
         toolbar: [['code-block']]  // Include button in toolbar
       },
       formats: ['code-block'],
@@ -47,8 +47,8 @@ class EditorContainer extends React.Component {
     // console.log('serverquill', serverquill);
 
 
-    document.getElementsByClassName('ql-code-block')[0].click();
-    document.getElementsByClassName('ql-toolbar')[0].remove();
+    // document.getElementsByClassName('ql-code-block')[0].click();
+    // document.getElementsByClassName('ql-toolbar')[0].remove();
     hljs.configure({   // optionally configure hljs
       languages: ['javascript']
     });
@@ -393,6 +393,7 @@ class EditorContainer extends React.Component {
   }
 
   type() {
+    window.clearInterval(this.typenow);
     if(this.props.quill.getText().length < 3){
       this.props.quill.updateContents({ops: [{insert: 'abcdefghijk'}]}, 'user');
     }
