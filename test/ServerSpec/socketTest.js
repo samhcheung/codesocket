edabc4e7cc11fa1d1b0db2c5d9eee60b0e0a3746
dadanili <yli523@gmail.com>
2016-10-11 09:56:27 -0700

    set up socket testing beforeEach and after Each

---
+++ test/ServerSpec/socketTest.js
@@ -0,0 +1,45 @@
+const expect = require('chai').expect;
+const {io} = require('../server/server.js');
+// const {User, Room} = require('../../../server/database/db-config');
+const ioClient = require('socket.io-client');
+const socketURL = 'http://0.0.0.0:3000';
+
+const options = {
+  transports: ['websocket'],
+  'force new connection': true
+};
+
+
+describe('Socket.io', () => {
+  var socket;
+  var socket2;
+  beforeEach(function(done) {
+    // Setup
+    socket = ioClient.connect('https://localhost:3000', {
+        'reconnection delay' : 0
+        , 'reopen delay' : 0
+        , 'force new connection' : true
+    });
+    socket.on('connect', function() {
+        console.log('worked...');
+        done();
+    });
+    socket.on('disconnect', function() {
+        console.log('disconnected...');
+    })
+  });
+
+  afterEach(function(done) {
+       // Cleanup
+       if(socket.connected) {
+           console.log('disconnecting...');
+           socket.disconnect();
+       } else {
+           // There will not be a connection unless you have done() in beforeEach, socket.on('connect'...)
+           console.log('no connection to break...');
+       }
+  done()
+   });
+
+
+})
