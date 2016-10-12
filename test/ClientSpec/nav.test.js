bd6f2c5c4f3ddd37cd054a09534b7365a815b26b
dadanili <yli523@gmail.com>
2016-10-11 23:39:28 -0700

    added test on Save editor button using sinon spy

--- test/ClientSpec/nav.test.js
+++ test/ClientSpec/nav.test.js
@@ -1,5 +1,5 @@
 jest.dontMock('../../src/client/app/editor/presentation.js');
-
+import chai from 'chai';
 import React from 'react';
 import renderer from 'react-test-renderer';
 var EditorPresentation = require('../../src/client/app/editor/presentation.js').default;
@@ -7,7 +7,7 @@ import {fromJS} from 'immutable';
 import {Provider} from 'react-redux';
 import sinon from 'sinon';
 import { mount, shallow } from 'enzyme';
-
+var expect = chai.expect;
 
 jest.mock('react/lib/ReactDefaultInjection')
 
@@ -16,10 +16,12 @@ describe('components', function() {
   	it('renders correctly', function() {
   		const onButtonClick = sinon.spy();
   		const wrapper = shallow(
-  		  <EditorPresentation saveCode={()=>{}} onClick={onButtonClick} />
+  		  <EditorPresentation saveCode={onButtonClick} />
   		);
-  		wrapper.find('#savebutton').simulate('click');
-  		expect(onButtonClick).to.have.property('callCount', 1);
+  		// var header = wrapper.find('div').at(0);
+  		wrapper.find('div').at(3).simulate('click');
+  		expect(onButtonClick.callCount).to.not.equal(0);
+
     });
   });
 });
